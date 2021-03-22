a408daf821415b8483e47ad01e0a1539506255d0
Arunasva (Ash) <43392585+ArunasvaArunasva@users.noreply.github.com>
2021-03-22 06:00:38 -0400

    display task functionality added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -20,7 +20,22 @@ def __repr__(self):
    
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method=='POST':
+        task_content = request.form['content']
+        new_task= Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Task cannot be added, there was an issue, oops!'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+    
 
 if __name__=="__main__":
     app.run(debug=True)
